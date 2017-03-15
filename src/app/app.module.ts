aee7caee3342c9faca099f015caa9e3f0f806462
morfat mosoti <morfatmosoti@gmail.com>
2017-03-15 14:54:13 +0300

    add admin LTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+//main app component 
 import { AppComponent } from './app.component';
 
-
-//other modules 
+//import other modules 
 import {DashboardModule} from './dashboard/dashboard.module';
-import { RouterModule } from '@angular/router';
+
 
 
 @NgModule({
